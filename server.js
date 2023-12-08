5c30dc0c91a2f5aa1e90968e37d0e6e379fe99af
davmagic <davidquish-1998@hotmail.com>
2023-12-07 21:38:02 -0500

    fix available deploy port on server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParse = require('body-parser')
 
-const PORT = 8000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
